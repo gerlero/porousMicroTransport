36c3b118c41df0710ffd984d2ba15f71ee0e8144
Gabriel Gerlero <gerlero@users.noreply.github.com>
2023-08-10 22:24:52 -0300

    Do not use parametrized fixtures with pytest-asyncio-cooperative

--- tests/test_validity/test_validity.py
+++ tests/test_validity/test_validity.py
@@ -4,14 +4,26 @@ from pathlib import Path
 
 DIR = Path(__file__).parent
 
-@pytest.fixture(scope="module", params=["LETxs", "LETd"])
-async def validity_case(run_case, request):
-    return await run_case(DIR / request.param)
+# Cannot use a parametrized fixture for these: https://github.com/willemt/pytest-asyncio-cooperative/issues/46
+@pytest.fixture(scope="module")
+async def letxs_case(run_case):
+    return await run_case(DIR / "LETxs")
+
+@pytest.fixture(scope="module")
+async def letd_case(run_case):
+    return await run_case(DIR / "LETd")
 
 
 @pytest.mark.asyncio_cooperative
-async def test_validity(validity_case):
-    actual = validity_case["100"]["theta"].getContent()["internalField"].value()
-    expected = validity_case["100"]["theta.expected"].getContent()["internalField"].value()
+async def test_letxs(letxs_case):
+    actual = letxs_case["100"]["theta"].getContent()["internalField"].value()
+    expected = letxs_case["100"]["theta.expected"].getContent()["internalField"].value()
+
+    assert actual == pytest.approx(expected, abs=5e-2)
+
+@pytest.mark.asyncio_cooperative
+async def test_letd(letd_case):
+    actual = letd_case["100"]["theta"].getContent()["internalField"].value()
+    expected = letd_case["100"]["theta.expected"].getContent()["internalField"].value()
 
     assert actual == pytest.approx(expected, abs=5e-2)
